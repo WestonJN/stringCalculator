d651a9a8e4e88f3dd9a11c205a74fef2f7ed7d4a
WestonJN <westonnyirenda@gmail.com>
2019-11-19 10:13:13 +0200

    changes to functions

--- src/calculator.js
+++ src/calculator.js
@@ -1,48 +1,49 @@
 class stringCalculator{
-    constructor() {
-        this.regex1 = /\d{1,}/gm;
-        this.negatives = (numbers) => {
-            
-                let str = '';
-                for (let i = 0; i > numbers.length; i++)
-                    if (numbers[i] === '-' && !isNaN(numbers[i + 1])) {
-                        str += '-' + numbers[i + 1] + '-'
-                    }
-                return str;
-        }
+   
+    negatives(string) {
+		let str = '';
+		let numbers = string.split(/\D/);
+
+		for (var i = 0; i < string.length; i++) {
+			if (string[i] == '-' && !isNaN(string[i+1])) {
+				str += numbers[i+1] +' ';
+			}
+		}
+
+		return str;
+	}
+
+    
+	add(numbers) {
+
+		let results = 0;
+		let negatives = this.negatives(numbers);
+
+		numbers = numbers.split(/\D/);
+
+		for (var i = 0; i < numbers.length; i++) {
+			
+			let number = parseInt(numbers[i]); 
+
+			
+		 	if (negatives) {
+		 		throw('Negative ' + negatives + 'not allowed');
+			}
+
+			
+		 	if (number > 1000) {
+		 		number = 0;
+			}
+			
+		 	if (number) {
+		 		
+				results += number;
+			}
+		}
+
+		return results;
     }
-
-    add(string) {
-
-        let numbers = string.match(this.regex1)
-        let results = 0;
-
-        if (string == "") {
-            return 0;
-        }
-        
-
-        let negative = this.negatives(numbers);
-        try {
-            if (string.includes('-')) 
-                throw ("Negative not allowed: -"+ numbers)
-                
-            } catch (e) { return e; }
-
-
-        for (let i = 0; i < numbers.length; i++) {
-            let digit = parseInt(numbers[i])
-                if (digit < 1000) {
-                    continue
-                }
-                if (digit) {
-                    results += digit;
-                }
-        }
-        return results;
-
-
-    } 
+    
         
 }
 
