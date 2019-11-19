dde8f5a266e5a0bd67ce74e6d477f7b0cd6b34d3
WestonJN <westonnyirenda@gmail.com>
2019-11-19 16:29:56 +0200

    Edited functions

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,35 +1,33 @@
 var calculator = require('../src/calculator');
 
-// const verify = new stringCalculator();
-
 describe("String calculator should", function () {
-  //  let verify = new stringCalculator;
+   let calculator = new stringCalculator;
 
     it(" equal to zero", function () {
-        expect(verify.add("")).toEqual(0);
+        expect(calculator.add("")).toEqual(0);
     });
  
     it(" equal  to one", function () {
-        expect(verify.add("1")).toEqual(1);
+        expect(calculator.add("1")).toEqual(1);
     });
 
     it(" equal 3", function () {
-        expect(verify.add("1,2")).toEqual(3);
+        expect(calculator.add("1,2")).toEqual(3);
     });
 
     it(" Numbers can be separated with \\n", function () {
-      expect(verify.add("1\n2\n3")).toEqual(6);
+      expect(calculator.add("1\n2\n3")).toEqual(6);
     });
 
     it("Ignore numbers greater or equal to 1000", () => {
-      expect(verify.add("1000,1")).toEqual(1);
+      expect(calculator.add("1000,1")).toEqual(1);
         });
 
     it("have multiple delimiter of length > 1", function () {
-      expect(verify.add("//[***]\n1***2***3")).toEqual(6);
+      expect(calculator.add("//[***]\n1***2***3")).toEqual(6);
     });
 
     it("More than one separators can be defined", function () {
-      expect(verify.add("//[*][%]\n1*2%3")).toEqual(6);
+      expect(calculator.add("//[*][%]\n1*2%3")).toEqual(6);
     });
   });
--- src/calculator.js
+++ src/calculator.js
@@ -1,38 +1,37 @@
 class stringCalculator{
-   
+
     negatives(string) {
-		let num = '';
+		let str = '';
 		let numbers = string.split(/\D/);
 
 		for (var i = 0; i < string.length; i++) {
-			if (string[i] == '-' && !isNaN(string[i+1])) {
-			 num += numbers[i+1] +' ';
+			if (string[i] === '-' && !isNaN(string[i+1])) {
+				str += numbers[i+1] +' ';
 			}
 		}
 
-		return num;
+		return str;
 	}
 
-    
-	add(numbers) {
+    add(numbers) {
 
 		let results = 0;
 		let negatives = this.negatives(numbers);
 
 		numbers = numbers.split(/\D/);
 
-		for (var i = 0; i < numbers.length; i++) {
+		for (let i = 0; i < numbers.length; i++) {
 			
 			let number = parseInt(numbers[i]); 
 
 			
 		 	if (negatives) {
-		 		throw('Negative not allowed: -' + negatives);
+		 		throw 'Negative not allowed: -'+ negatives;
 			}
 
 			
-		 	if (number > 1000) {
-		 		number = 0;
+		 	if (number >= 1000) {
+		 	  number = 0
 			}
 			
 		 	if (number) {
@@ -42,15 +41,12 @@ class stringCalculator{
 		}
 
 		return results;
-    }
-    
-        
+	}
+      
 }
+
 
 let calculator = new stringCalculator();
 console.log(calculator.add('//[*][%]\n1*2%3'));
 console.log(calculator.add(''));
-console.log(calculator.add('-5'));
-console.log(calculator.add('1000,3'));
-console.log(calculator.add('1\n2\n3'))
  module.exports = {stringCalculator}
